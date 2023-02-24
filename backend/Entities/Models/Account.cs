32ef048711e79df71efc725b4b5d314af69f16fd
KauanMP <kauanmoreira1232@gmail.com>
2023-02-23 22:27:04 -0300

    Teste de Log; Criação do Projeto de biblioteca de classe Entities para as entidades e contexto

---
+++ backend/Entities/Models/Account.cs
@@ -0,0 +1,19 @@
+namespace Entities.Models
+{
+    [Table("Account")]
+    public class Account
+    {
+        [Key]
+        public Guid AccountId { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string AccountType { get; set; }
+        [ForeignKey(nameof(Owner))]
+        public Guid OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
